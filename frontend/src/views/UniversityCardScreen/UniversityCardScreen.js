49b1a7cb39275c393ccf479c3c70ae915d63cf77
siddharth <jsidd06@gmail.com>
2022-02-15 12:14:08 +0530

    now set the unviersity enter face and use serach option and dynamic tag bar

--- frontend/src/views/UniversityCardScreen/UniversityCardScreen.js
+++ frontend/src/views/UniversityCardScreen/UniversityCardScreen.js
@@ -1,29 +1,55 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Card, CardBody, Col, Container, Input, Row, Table } from 'reactstrap'
 
 function UniversityCardScreen() {
-    const [universities, setUniversities] = React.useState([])
-    const [isLoading, setIsLoading] = React.useState(true)
-    const universitieCheck = () => {
+    const [universities, setUniversities] = useState([])
+    const [search, setSearch] = useState('')
+    const [isLoading, setIsLoading] = useState(true)
+    const universitiesHandler = () => {
         axios
           .get(`http://universities.hipolabs.com/search?country=United+States`)
           .then((res) => {
               console.log(res.data)
             setUniversities(res.data);
-            setIsLoading(false);
+            setIsLoading(false)
           });
     }
   return (
     <>
-      <div>UniversityCardScreen</div>
-      <button onClick={universitieCheck}> submit</button>
-      {universities.map((university) => (
-        <div>
-            <div>{university.name}</div>
-            <div>{university.country}</div>
-            <div>{university.web_pages[0]}</div>
-            </div>
-      ))}
+      <Container>
+        <h1>UniversityCardScreen</h1>
+        <Input
+          placeholder="enter your university name"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Button onClick={universitiesHandler}>Check Now</Button>
+        {universities &&
+          universities
+            .filter((university) =>
+              university.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((university) => (
+              <Card>
+                <CardBody>
+                  <Row>
+                    <Col md="4">
+                      <h1>{university.name}</h1>
+                    </Col>
+                    <Col md="4">
+                      <h1>{university.country}</h1>
+                    </Col>
+                    <Col md="4">
+                      <a href={`${university.web_pages}`} target="_blank">
+                        {university.web_pages}
+                      </a>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            ))}
+      </Container>
     </>
   );
 }
