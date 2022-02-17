76e3652f4f8bbefcb2e7722a2f39ad3d801fbe1f
siddharth <jsidd06@gmail.com>
2022-02-17 14:18:44 +0530

    add some changes in quotes file

--- frontend/src/views/QuotesScreen/Quotes.js
+++ frontend/src/views/QuotesScreen/Quotes.js
@@ -17,9 +17,21 @@ function QuotesScreen() {
 
   return (
     <Container className="text-center mt-5 quotesBody">
-      <h1>Welcome to Quotes World</h1>
+      <h1 className="newOne">Welcome to Quotes World!</h1>
+      {quotes.content ? (
+        <>
+          <Container className="text-center mt-3">
+            <Card className="mt-3 p-3">
+              <h2 className="newStyle p-3">{quotes.content}</h2>
+              <h4 className="cursorStyle p-2">Author Name:- {quotes.author}</h4>
+            </Card>
+          </Container>
+        </>
+      ) : (
+        <h1>No Quotes Found 🤔</h1>
+      )}
       <Button
-        className="mt-2 mb-4"
+        className="mt-4 mb-4"
         style={{ paddingLeft: 50, paddingRight: 50 }}
         color="primary"
         onClick={submitHandler}
@@ -27,14 +39,6 @@ function QuotesScreen() {
       >
         New Quote
       </Button>
-      {quotes.content ? ( 
-        <>
-          <h1 className="newStyle">{quotes.content}</h1>
-          <h4 className="cursorStyle">Author Name:- {quotes.author}</h4>
-        </>
-      ) : (
-        <h1>No Quotes Found 🤔</h1>
-      )}
     </Container>
   );
 }
