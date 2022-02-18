ed96028f19c0ecb60ac35c22d2a241d594e2ad3d
siddharth <jsidd06@gmail.com>
2022-02-18 20:08:01 +0530

    set the loading in ipgeo

--- frontend/src/views/IpGeo/IpGeo.js
+++ frontend/src/views/IpGeo/IpGeo.js
@@ -3,11 +3,14 @@ import { Card, CardBody, CardHeader, Container } from "reactstrap";
 import axios from "axios";
 function IpGeo() {
   const [info, setInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get("https://api.techniknews.net/ipgeo/49.36.217.125")
       .then((res) => {
         setInfo(Object.entries(res.data));
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
@@ -20,7 +23,13 @@ function IpGeo() {
           <h3>Info-About-You</h3>
         </CardHeader>
         <CardBody>
-          {info.map(([key, value], index) => (
+          {isLoading ?  <div className="mt-5 text-center p-3">
+            <i
+              style={{ fontSize: 50 }}
+              className="fas text-success fa-spinner fa-spin mb-4"
+            ></i>
+            <h5>Please Wait ✋ We are Fetching Your Data..</h5>
+          </div> : info && info.map(([key, value], index) => (
             <Card key={index} className="p-4">
               <h5>
                 {key} : {value || "N/A"}
